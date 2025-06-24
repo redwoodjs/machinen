a1a6d5ffa8fb6660d47d494acb601d78be3f472d
justinvdm <justinvderm@gmail.com>
2025-06-24 14:19:19 +0200

    enter working

--- src/app/pages/editor/Preview.tsx
+++ src/app/pages/editor/Preview.tsx
@@ -5,25 +5,26 @@ export const Preview = () => {
   const [input, setInput] = useState("/");
   const [src, setSrc] = useState("/");
 
-  const handleGo = () => {
+  const handleGo = (e: React.FormEvent) => {
+    e.preventDefault();
     setSrc(input);
   };
 
   return (
     <>
-      <input
-        className="text-sm flex-1 border rounded px-2 py-1"
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        spellCheck={false}
-      />
-      <button
-        onClick={handleGo}
-        className="text-sm px-3 py-1 border rounded bg-gray-100 hover:bg-gray-200"
-      >
-        Go
-      </button>
+      <form onSubmit={handleGo} className="flex gap-2 mb-2">
+        <input
+          className="text-sm flex-1 border rounded px-2 py-1"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          spellCheck={false}
+        />
+        <button type="submit" className="text-sm px-3 py-1 border rounded">
+          Go
+        </button>
+      </form>
+
       <iframe src={`/preview${src}`} className="h-full w-full border rounded" />
     </>
   );
